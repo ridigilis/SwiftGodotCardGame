168765828d61b8105d161e4417bc1fa1f768dc5a
ridigilis <ridigilis@ridigilis.dev>
2024-03-20 15:57:02 -0500

    add drag min threshold to dragging state to prevent occasional unintended snap-back behavior

--- SwiftLand/Sources/scenes/CardDraggingState.swift
+++ SwiftLand/Sources/scenes/CardDraggingState.swift
@@ -2,6 +2,9 @@ import SwiftGodot
 
 @Godot
 final class CardDraggingState: CardState {
+	let DRAG_MINIMUM_THRESHOLD = 0.05
+	var minimumDragTimeElapsed = false
+
 	required init() {
 		super.init()
 		state = CardState.State.dragging.rawValue
@@ -14,26 +17,33 @@ final class CardDraggingState: CardState {
 
 	override func enter() {
 		if let card = cardUI {
-		if let uiLayer = getTree()?.getFirstNodeInGroup("ui_layer") {
-			card.reparent(newParent: uiLayer)
-		}
-		card.Color.color = Color.blue
-		card.State.text = "DRAGGING"
+			var thresholdTimer = getTree()?.createTimer(timeSec: DRAG_MINIMUM_THRESHOLD, processAlways: false)
+			if let uiLayer = getTree()?.getFirstNodeInGroup("ui_layer") {
+				card.reparent(newParent: uiLayer)
+			}
+			card.Color.color = Color.blue
+			card.State.text = "DRAGGING"
+
+			minimumDragTimeElapsed = false
+			thresholdTimer?.timeout.connect { self.minimumDragTimeElapsed = true }
 		}
 	}
 
 	override func onInput(event: InputEvent) {
+		// mouse motion
 		if event is InputEventMouseMotion {
 			if let card = cardUI {
 			card.setGlobalPosition(card.getGlobalMousePosition() - card.pivotOffset)
 			}
 		}
 
+		// cancel
 		if event.isActionPressed(action: "right_mouse") {
 			emit(signal: CardState.transitionRequested, self, CardState.State.base.rawValue)
 		}
 
-		if event.isActionReleased(action: "left_mouse") || event.isActionPressed(action: "left_mouse") {
+		// confirm
+		if minimumDragTimeElapsed && (event.isActionReleased(action: "left_mouse") || event.isActionPressed(action: "left_mouse")) {
 			getViewport()?.setInputAsHandled()
 			emit(signal: CardState.transitionRequested, self, CardState.State.released.rawValue)
 		}
